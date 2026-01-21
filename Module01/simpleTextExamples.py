00096ee2addee57b026ec141ea76e503da87bf30
GRDavies75 <gdavies@home.nl>
2026-01-21 16:41:59 +0100

    more text examples and other commands

--- Module01/simpleTextExamples.py
+++ Module01/simpleTextExamples.py
@@ -26,7 +26,7 @@ def main():
     #title
     st.title("This is a Title")
 
-    #markdown
+    # markdown
     st.markdown("## This is markdown")
 
     #displaying Text with Colored background / highlights
@@ -35,6 +35,18 @@ def main():
     st.warning("This is dangerous") # Yellow background
     st.error("This is an error") # Red background
     st.exception("This is an exception") # Also Red background # type: ignore
+
+    # superfunction - it tries to render it in the "intended way"
+    st.write("This is a normal text (through .write)")
+    st.write("## This is a markdown text (through .write)")
+    st.write(1 + 2)
+
+    # Show all functionality of Streamlit
+    st.write("### Available methods / functions / etc within Streamlit.")
+    st.write(dir(st))
+
+    # Help function
+    st.help(range)
 
 
 if __name__ == '__main__':
